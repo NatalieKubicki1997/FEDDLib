2fc61240b2119bf2f97b1c9907c422414e1922dc
NatalieKubicki1997 <nkubicki@uni-koeln.de>
2022-07-21 08:54:28 +0200

    Where is steadyCreepingMotionNonNewtonianAssFE

---
+++ feddlib/problems/tests/steadyCreepingMotionNonNewtonianAssFE/main.cpp
@@ -0,0 +1,372 @@
+#ifndef MAIN_TIMER_START
+#define MAIN_TIMER_START(A,S) Teuchos::RCP<Teuchos::TimeMonitor> A = Teuchos::rcp(new Teuchos::TimeMonitor(*Teuchos::TimeMonitor::getNewTimer(std::string("Main") + std::string(S))));
+#endif
+
+#ifndef MAIN_TIMER_STOP
+#define MAIN_TIMER_STOP(A) A.reset();
+#endif
+
+#include "feddlib/core/FEDDCore.hpp"
+#include "feddlib/core/Mesh/MeshPartitioner.hpp"
+#include "feddlib/core/FE/Domain.hpp"
+#include "feddlib/core/General/DefaultTypeDefs.hpp"
+#include "feddlib/core/General/ExporterParaView.hpp"
+#include "feddlib/core/LinearAlgebra/MultiVector.hpp"
+
+#include "feddlib/problems/Solver/NonLinearSolver.hpp"
+#include "feddlib/problems/specific/NavierStokesAssFE.hpp"
+
+#include <Teuchos_GlobalMPISession.hpp>
+#include <Xpetra_DefaultPlatform.hpp>
+
+
+/*!
+ main of steady-state Creeping flow problem with Non-Newtonian stress tensor assumption 
+ where we use e.g. Carreau Yasuda or power law
+
+ @brief steady-state Non-Newtonian creeping Flow main
+ @author Natalie Kubicki
+ @version 1.0
+ @copyright NK
+ */
+
+using namespace std;
+using namespace Teuchos;
+using namespace FEDD;
+
+void zeroDirichlet(double* x, double* res, double t, const double* parameters){
+
+   // res[0] = 0.;
+    res[0] = 0.0;
+    return;
+}
+
+void zeroDirichlet2D(double* x, double* res, double t, const double* parameters){
+
+    res[0] = 0.;
+    res[1] = 0.;
+
+    return;
+}
+
+void couette2D(double* x, double* res, double t, const double* parameters){
+
+    res[0] = parameters[0]; // da in parameters 0 maxvelocity
+    res[1] = 0.;
+
+    return;
+}
+
+
+void one(double* x, double* res, double t, const double* parameters){
+
+    res[0] = 1.;
+    res[1] = 1.;
+
+    return;
+}
+void two(double* x, double* res, double t, const double* parameters){
+
+    res[0] = 2.;
+    res[1] = 2.;
+
+    return;
+}
+void three(double* x, double* res, double t, const double* parameters){
+
+    res[0] = 3.;
+    res[1] = 3.;
+
+    return;
+}
+void four(double* x, double* res, double t, const double* parameters){
+
+    res[0] = 4.;
+    res[1] = 4.;
+
+    return;
+}
+
+void zeroDirichlet3D(double* x, double* res, double t, const double* parameters){
+
+    res[0] = 0.;
+    res[1] = 0.;
+    res[2] = 0.;
+
+    return;
+}
+
+void inflowParabolic2D(double* x, double* res, double t, const double* parameters){
+   
+  
+
+       /* double H = parameters[1];
+    res[0] = 4.*parameters[0]*x[1]*(H-x[1])/(H*H);
+    res[1] = 0.;
+*/
+        //double H = parameters[1];
+    double H = 0.1;
+    double n = 0.6;
+    double rho_ref = 1000.0;
+    double nu_0 = 0.035;
+    double dp = 10000.0;
+
+    // res[0] = 4*parameters[0]*x[1]*(H-x[1])/(H*H);
+    // For v = 0.01 with rho=1000 so mu = 10
+    // and dp/dx should be= -10^4
+    res[0] = (n/(n+1.0))*pow( dp/(rho_ref*nu_0), 1.0/n)*( pow( H/(2.0), (n+1.0)/n) - pow( abs( (H/2.0) -x[1] ) , (n+1.0)/n  ) );
+    //*10.0;
+    res[1] = 0.;
+
+     return;
+    
+}
+
+void inflowParabolic3D(double* x, double* res, double t, const double* parameters){
+
+    double H = parameters[1];
+    res[0] = 16*parameters[0]*x[1]*(H-x[1])*x[2]*(H-x[2])/(H*H*H*H);
+    res[1] = 0.;
+    res[2] = 0.;
+
+    return;
+}
+void inflow3DRichter(double* x, double* res, double t, const double* parameters)
+{
+
+    double H = parameters[1];
+    
+    res[0] = 9./8 * parameters[0] *x[1]*(H-x[1])*(H*H-x[2]*x[2])/( H*H*(H/2.)*(H/2.) );
+    res[1] = 0.;
+    res[2] = 0.;
+
+    
+    return;
+}
+void dummyFunc(double* x, double* res, double t, const double* parameters){
+
+    return;
+}
+
+
+typedef unsigned UN;
+typedef default_sc SC;
+typedef default_lo LO;
+typedef default_go GO;
+typedef default_no NO;
+
+using namespace FEDD;
+
+int main(int argc, char *argv[]) {
+    
+    typedef MeshPartitioner<SC,LO,GO,NO> MeshPartitioner_Type;
+    typedef Teuchos::RCP<Domain<SC,LO,GO,NO> > DomainPtr_Type;
+
+    typedef Matrix<SC,LO,GO,NO> Matrix_Type;
+    typedef Teuchos::RCP<Matrix_Type> MatrixPtr_Type;
+
+    Teuchos::oblackholestream blackhole;
+    Teuchos::GlobalMPISession mpiSession(&argc,&argv,&blackhole);
+
+    Teuchos::RCP<const Teuchos::Comm<int> > comm = Xpetra::DefaultPlatform::getDefaultPlatform().getComm();
+    bool verbose (comm->getRank() == 0);
+
+//    Teuchos::RCP<Teuchos::FancyOStream> out = Teuchos::VerboseObjectBase::getDefaultOStream();
+
+    if (verbose) {
+        cout << "###############################################################" <<endl;
+        cout << "##################### Steady Non-Newtonian Creeping Flow ####################" <<endl;
+        cout << "###############################################################" <<endl;
+    }
+
+    // Command Line Parameters
+    Teuchos::CommandLineProcessor myCLP;
+
+    string xmlProblemFile = "parametersProblem.xml";
+    myCLP.setOption("problemfile",&xmlProblemFile,".xml file with Inputparameters.");
+    string xmlPrecFile = "parametersPrec.xml";
+    myCLP.setOption("precfile",&xmlPrecFile,".xml file with Inputparameters.");
+    string xmlSolverFile = "parametersSolver.xml";
+    myCLP.setOption("solverfile",&xmlSolverFile,".xml file with Inputparameters.");
+
+    string xmlTekoPrecFile = "parametersTeko.xml";
+    myCLP.setOption("tekoprecfile",&xmlTekoPrecFile,".xml file with Inputparameters.");
+
+    double length = 4.;
+    myCLP.setOption("length",&length,"length of domain.");
+
+    myCLP.recogniseAllOptions(true);
+    myCLP.throwExceptions(false);
+    Teuchos::CommandLineProcessor::EParseCommandLineReturn parseReturn = myCLP.parse(argc,argv);
+    if(parseReturn == Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED) {
+        MPI_Finalize();
+        return 0;
+    }
+
+    {
+        ParameterListPtr_Type parameterListProblem = Teuchos::getParametersFromXmlFile(xmlProblemFile);
+
+        ParameterListPtr_Type parameterListPrec = Teuchos::getParametersFromXmlFile(xmlPrecFile);
+
+        ParameterListPtr_Type parameterListSolver = Teuchos::getParametersFromXmlFile(xmlSolverFile);
+
+        ParameterListPtr_Type parameterListPrecTeko = Teuchos::getParametersFromXmlFile(xmlTekoPrecFile);
+
+        int 		dim				= parameterListProblem->sublist("Parameter").get("Dimension",3);
+
+
+        bool        newtonian       = parameterListProblem->sublist("Material").get("Newtonian",true);
+        std::string discVelocity = parameterListProblem->sublist("Parameter").get("Discretization Velocity","P2");
+        std::string discPressure = parameterListProblem->sublist("Parameter").get("Discretization Pressure","P1");
+
+
+        string		meshType    	= parameterListProblem->sublist("Parameter").get("Mesh Type","structured");
+        string		meshName    	= parameterListProblem->sublist("Parameter").get("Mesh Name","circle2D_1800.mesh");
+        string		meshDelimiter   = parameterListProblem->sublist("Parameter").get("Mesh Delimiter"," ");
+        int 		m				= parameterListProblem->sublist("Parameter").get("H/h",5);
+        string		linearization	= parameterListProblem->sublist("General").get("Linearization","FixedPoint");
+        string		precMethod      = parameterListProblem->sublist("General").get("Preconditioner Method","Monolithic");
+        int         mixedFPIts		= parameterListProblem->sublist("General").get("MixedFPIts",1);        
+        int         n;
+
+        // Einlesen von Parameterwerten bezüglich des ausgewählten Material Gesetzes
+
+
+
+
+        ParameterListPtr_Type parameterListAll(new Teuchos::ParameterList(*parameterListProblem)) ;
+        if (!precMethod.compare("Monolithic"))
+            parameterListAll->setParameters(*parameterListPrec);
+        else
+            parameterListAll->setParameters(*parameterListPrecTeko);
+
+        parameterListAll->setParameters(*parameterListSolver);    
+        
+        bool        newtonianx       = parameterListAll->sublist("Material").get("Newtonian",true);
+        std::string bcType = parameterListProblem->sublist("Parameter").get("BC Type","parabolic");
+
+        int minNumberSubdomains;
+        if (!meshType.compare("structured")) {
+            minNumberSubdomains = 1;
+        }
+        else if(!meshType.compare("structured_bfs")){
+            minNumberSubdomains = (int) 2*length+1;
+        }
+
+        int numProcsCoarseSolve = parameterListProblem->sublist("General").get("Mpi Ranks Coarse",0);
+        int size = comm->getSize() - numProcsCoarseSolve;
+
+        {
+            DomainPtr_Type domainPressure;
+            DomainPtr_Type domainVelocity;
+                              
+            domainPressure.reset( new Domain<SC,LO,GO,NO>( comm, dim ) );
+            domainVelocity.reset( new Domain<SC,LO,GO,NO>( comm, dim ) );
+            
+            MeshPartitioner_Type::DomainPtrArray_Type domainP1Array(1);
+            domainP1Array[0] = domainPressure;
+            
+            ParameterListPtr_Type pListPartitioner = sublist( parameterListProblem, "Mesh Partitioner" );
+            MeshPartitioner<SC,LO,GO,NO> partitionerP1 ( domainP1Array, pListPartitioner, "P1", dim );
+            
+            partitionerP1.readAndPartition();
+
+            if (discVelocity=="P2")
+                domainVelocity->buildP2ofP1Domain( domainPressure );
+            else
+                domainVelocity = domainPressure;
+      
+            std::vector<double> parameter_vec(1, parameterListProblem->sublist("Parameter").get("MaxVelocity",1.));
+
+
+
+
+
+
+
+
+
+//          **********************  BOUNDARY CONDITIONS ***********************************
+            // ####################
+            Teuchos::RCP<BCBuilder<SC,LO,GO,NO> > bcFactory( new BCBuilder<SC,LO,GO,NO>( ) );
+          	 parameter_vec.push_back(1);//0.41);//height of inflow region
+
+
+//** SO THIS WAS FOR COUETTE FLOW
+        /*    if (dim==2){
+            // So for Couette Flow we have a moving upper plate and rigid lower plate  
+                bcFactory->addBC(zeroDirichlet2D, 1, 0, domainVelocity, "Dirichlet", dim); // wall
+                bcFactory->addBC(couette2D, 2, 0, domainVelocity, "Dirichlet", dim, parameter_vec); // wall
+            // The flow will be induced by the moving plate so there should be no pressure gradient in this testcase
+                bcFactory->addBC(zeroDirichlet2D, 3, 1, domainPressure, "Dirichlet", 1); // outflow
+                bcFactory->addBC(zeroDirichlet2D, 4, 1, domainPressure, "Dirichlet", 1); // inflow
+            }
+*/
+ //*** POISEUILLE FLOW
+                bcFactory->addBC(zeroDirichlet2D, 1, 0, domainVelocity, "Dirichlet", dim); // wall
+                bcFactory->addBC(zeroDirichlet2D, 2, 0, domainVelocity, "Dirichlet", dim, parameter_vec); // wall
+                //bcFactory->addBC(, 3, 0, domainVelocity, "Dirichlet_Z", dim); // flag 3 is neumann 0, müssen wir nicht explizit angeben, da das defaulr ist 
+                //bcFactory->addBC(zeroDirichlet3D, 4, 0, domainVelocity, "Dirichlet", dim);
+              
+                 bcFactory->addBC(inflowParabolic2D, 4, 0, domainVelocity, "Dirichlet", dim, parameter_vec); 
+                // bcFactory->addBC(zeroDirichlet, 3, 1, domainPressure, "Dirichlet", 1); //Outflow
+           //    bcFactory->addBC(FixedDirichlet, 4, 1, domainPressure, "Dirichlet", 1, parameter_vec); // inflow
+           // FESTLEGEN VON WERT AM RECHTEN RAND GEHT NICHT
+                           //  bcFactory->addBC(zeroDirichlet, 4, 1, domainPressure, "Dirichlet", 1, parameter_vec); // inflow
+
+
+
+
+            //          **********************  CALL SOLVER ***********************************
+			// ---------------------
+		
+
+           	// ---------------------
+			// New Assembly Rouine
+        
+            NavierStokesAssFE<SC,LO,GO,NO> navierStokesAssFE( domainVelocity, discVelocity, domainPressure, discPressure, parameterListAll );
+
+            {
+				MAIN_TIMER_START(NavierStokesAssFE," AssFE:   Assemble System and solve");
+                navierStokesAssFE.addBoundaries(bcFactory);
+                navierStokesAssFE.initializeProblem();
+                navierStokesAssFE.assemble();
+
+                navierStokesAssFE.setBoundariesRHS();
+
+                std::string nlSolverType = parameterListProblem->sublist("General").get("Linearization","FixedPoint");
+                NonLinearSolver<SC,LO,GO,NO> nlSolverAssFE( nlSolverType );
+                nlSolverAssFE.solve( navierStokesAssFE );
+				MAIN_TIMER_STOP(NavierStokesAssFE);	
+                comm->barrier();
+            }
+ 
+
+ //          **********************  POST-PROCESSING ***********************************
+			Teuchos::TimeMonitor::report(cout,"Main");	
+       
+			Teuchos::RCP<ExporterParaView<SC,LO,GO,NO> > exParaVelocity(new ExporterParaView<SC,LO,GO,NO>());
+            Teuchos::RCP<ExporterParaView<SC,LO,GO,NO> > exParaPressure(new ExporterParaView<SC,LO,GO,NO>());
+
+			
+			
+            Teuchos::RCP<const MultiVector<SC,LO,GO,NO> > exportSolutionVAssFE = navierStokesAssFE.getSolution()->getBlock(0);
+            Teuchos::RCP<const MultiVector<SC,LO,GO,NO> > exportSolutionPAssFE = navierStokesAssFE.getSolution()->getBlock(1);
+
+            DomainPtr_Type dom = domainVelocity;
+            exParaVelocity->setup("velocity", dom->getMesh(), dom->getFEType());                
+            UN dofsPerNode = dim;
+            exParaVelocity->addVariable(exportSolutionVAssFE, "uAssFE", "Vector", dofsPerNode, dom->getMapUnique());
+
+            dom = domainPressure;
+            exParaPressure->setup("pressure", dom->getMesh(), dom->getFEType());
+            exParaPressure->addVariable(exportSolutionPAssFE, "pAssFE", "Scalar", 1, dom->getMapUnique());
+
+
+            exParaVelocity->save(0.0);
+            exParaPressure->save(0.0); 
+        }
+    }
+    Teuchos::TimeMonitor::report(cout);
+    return(EXIT_SUCCESS);
+}
